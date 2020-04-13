e0973e26f6b694ef35b43df7c38e0890078370f2
Jake Nemiroff <jakenemiroff@gmail.com>
2020-04-13 17:16:01 -0400

    clean up code and add header component

---
+++ src/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+
+    return (
+        <div>
+            hello world
+        </div>
+    );
+}
+
+export default Header;
