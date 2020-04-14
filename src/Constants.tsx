d4eb87abcc77ea6838536f6cefd29110c589e6df
Jake Nemiroff <jakenemiroff@gmail.com>
2020-04-13 22:07:34 -0400

    create constants file for strings - update header with some initial styling

---
+++ src/Constants.tsx
@@ -0,0 +1,4 @@
+export const ABOUT = 'About'
+export const WORK = 'Work'
+export const PROJECTS = 'Projects'
+export const CONTACT = 'Contact'
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as Constants from '../Constants';
+
+const StyledHeader = styled.div`
+
+    height: 100px;
+    width: inherit;
+    border: solid 1px black;
+`;
+
+const Banner = styled.div`
+    
+    display: flex; 
+    height: inherit;
+    float: right;
+`;
+
+const NavText = styled.p`
+    
+    margin: auto; /* Important */
+    font-family: 'Ubuntu Mono', monospace;
+    padding-right: 20px;
+`;
 
 const Header = () => {
 
     return (
-        <div>
-            hello world
-        </div>
+        <StyledHeader>
+
+            <Banner>
+                <NavText>{Constants.ABOUT}</NavText>
+                <NavText>{Constants.WORK}</NavText>
+                <NavText>{Constants.PROJECTS}</NavText>
+                <NavText>{Constants.CONTACT}</NavText>
+            </Banner>
+
+        </StyledHeader>
     );
 }
 
